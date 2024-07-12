d015f33a3c06da8f29877e05ca720b05c1407ab5
kfinn315 <kfinn315@gmail.com>
2024-07-12 15:13:18 -0400

    refactor SunburstItemHierarchyNode folder

--- src/Components/SunburstItemHierarchyNode/SunburstItemHierarchyNode.ts
+++ src/Components/SunburstItemHierarchyNode/SunburstItemHierarchyNode.ts
@@ -1,12 +1,28 @@
-import { HierarchyRectangularNode } from "d3";
+import { HierarchyNode, HierarchyRectangularNode } from 'd3';
 import { SunburstItem, SunburstItemTreeNode } from "../Types";
 import BoxDimensions from "../../Shared/BoxDimensions";
 import createTree from "../../Tree/Tree";
-import { getSunburstHierarchyRectangularNode as getSunburstHierarchyRectangularNode } from "./getSunburstHierarchyRectangularNode";
-import { convertToSegmentNode } from "./convertToSegmentNode";
+import { SegmentNode } from "../../Tree/Types";
+import { TreePartitionLayout } from '../../PartitionLayout/TreePartitionLayout';
 
+
+function getSegments(item: SunburstItem): string[] {
+    return item.name.split('.');
+}
+
+function getSunburstItemNodeValue(d: SunburstItemTreeNode) {
+    return d.data?.size ?? 0;
+}
+
+function sortSunburstItemByDataSize(nodeA: HierarchyNode<SunburstItemTreeNode>, nodeB: HierarchyNode<SunburstItemTreeNode>) {
+    return (nodeB.data.data?.size ?? 0) - (nodeA.data.data?.size ?? 0);
+}
+
+export function getSunburstHierarchyRectangularNode(root: SunburstItemTreeNode, dimensions: BoxDimensions): HierarchyRectangularNode<SunburstItemTreeNode> {
+    return TreePartitionLayout<SunburstItem>(root, dimensions, getSunburstItemNodeValue, sortSunburstItemByDataSize);
+}
 export default function SunburstItemHierarchyNode(items: SunburstItem[], layoutDimensions: BoxDimensions): HierarchyRectangularNode<SunburstItemTreeNode> {
-    const segmentNodes = items.map(item => convertToSegmentNode(item))
+    const segmentNodes: Array<SegmentNode<SunburstItem>> = items.map(item => ({ data: item, segments: getSegments(item) }))
     const rootTreeNode = createTree<SunburstItem>(segmentNodes)
     return getSunburstHierarchyRectangularNode(rootTreeNode, layoutDimensions)
 }
